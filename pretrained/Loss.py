431fc9b64f2d710cb730ddfbdce838b91157599a
cerredz <422michaelcerreto@gmail.com>
2025-09-16 20:31:11 -0400

    think i made the loss function better, am getting there

--- pretrained/Loss.py
+++ pretrained/Loss.py
@@ -2,15 +2,29 @@ import torch
 
 def loss(predictions, targets, lambda_coord=5, S=7, B=2, C=20):
 
-    l1 = 0
-    for i in range(0, S ** 2):
-        for j in range(0, B):
-            x_idx = 20 + (j * 5):21 + (j * 5)
-            y_idx = 21 + (j * 5):22 + (j * 5)
+    l1, l2, l3, l4, l5 = 0, 0, 0, 0, 0
 
-            loss_x_squared = pow((targets[:,:,:,x_idx] - predictions[:,:,:,x_idx], 2))
-            loss_y_squared = pow((targets[:,:,:,y_idx] - predictions[:,:,:,y_idx], 2))
-            l1 += (loss_x_squared + loss_y_squared)
+    # calculate the first loss term (x and y)
+    for pred in predictions:
+        s_idx_1, s_idx_2 = -1, -1
+        for i in range(0, S ** 2):
+            s_idx_2 += 1
+            if s_idx_2 == S:
+                s_idx_2 = 0
+                s_idx_1 += 1
+            for j in range(0, B):
+                x_idx = C + (j * 5):C + 1 + (j * 5)
+                y_idx = C + 1 + (j * 5):C + 2 + (j * 5)
+
+                loss_x_squared = pow((targets[:,s_idx_1,s_idx_2,x_idx] - predictions[:,s_idx_1,s_idx_2,x_idx], 2))
+                loss_y_squared = pow((targets[:,s_idx_1,s_idx_2,y_idx] - predictions[:,s_idx_1,s_idx_2,y_idx], 2))
+                l1 += (loss_x_squared + loss_y_squared)
+
+    l1 *= lambda_coord
+
+    # calculate the second loss term (w and h)
+    for i in range(0, S ** 2):
+
 
 
 
